19449e73822e9af1058726cf91325fa701589b05
rosskeim <rosskeim@gmail.com>
2021-03-11 13:58:57 -0700

    latest update

--- prog.py
+++ prog.py
@@ -5,52 +5,44 @@ class TreeNode(object):
     self.left = left
     self.right = right
  
-  def print_tree(self, traversal):
-    if self.left:
-      traversal.append(self.val)
-      self.print_tree(self.left, traversal)
-    elif self.right:
-      traversal.append(self.val)
-      self.print_tree(self.right, traversal)
+  def print_tree(self, root, traversal=[]):
+    
+    traversal.append(root.val)
+    
+    if root.left:
+        if root.right:
+          traversal = self.print_tree(root.right, traversal)
 
     return traversal 
 
 class Solution(object):
-  def mergeTrees(self, root1, root2, traversal=[]):
+  def mergeTrees(self, root1, root2):
     """
     :type root1: TreeNode
     :type root2: TreeNode
     :rtype: TreeNode
     """
-    
-    if root1:
-      if root2:
-        traversal.append(root1.val+root2.val)
-      else
-        traversal.append(root1.val)
-    elif root2:
-      traversal.append(root2.val)
+    if root1 is None:
+      return root2
 
-    if root1.left:
-      if root2.left:
-        root1.left = self.mergeTrees(root1.left, root2.left)
-      else:
-        root1.left = self.mergeTrees(root1.left, TreeNode())
-    elif root1.right:
-      if root2.right:
-        root1.right = self.mergeTrees(root1.right, root2.right)
-      else:
-        root1.right = self.mergeTrees(root1.right, TreeNode())
+    if root2 is None:
+      return root1
 
-    return TreeNode(root1.val+root2.val) 
+    root1.val += root2.val
+    root1.left = self.mergeTrees(root1.left, root2.left)
+    root1.right = self.mergeTrees(root1.right, root2.right)
+
+    return root1
 
 obj = Solution()
 
+# [1,3,2,5]
 root1 = TreeNode(1)
 root1.left = TreeNode(3)
 root1.left.left = TreeNode(5)
 root1.right = TreeNode(2)
 
+# [2,1,3,null,4,null,7]
 root2 = TreeNode(2)
 root2.left = TreeNode(1)
 root2.left.right = TreeNode(4)
@@ -59,6 +51,5 @@ root2.right.right = TreeNode(7)
 
 merged = obj.mergeTrees(root1, root2)
 
-traversal = [3]
-print(root2.left.right.val)
-print(merged.print_tree(traversal))
+print(type(merged.right.left.val))
+print(merged.print_tree(merged))
