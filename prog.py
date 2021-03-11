cd8428686dfa5d9d4d08b37e2f0af10e01640843
Ross Keim <rkeim@Rosss-Air.hitronhub.home>
2021-03-10 18:41:12 -0700

    treenode close

--- prog.py
+++ prog.py
@@ -4,32 +4,61 @@ class TreeNode(object):
     self.val = val
     self.left = left
     self.right = right
-  
-  def print_tree():
+ 
+  def print_tree(self, traversal):
     if self.left:
-      self.left.print_tree()
+      traversal.append(self.val)
+      self.print_tree(self.left, traversal)
     elif self.right:
-      self.right.print_tree()
+      traversal.append(self.val)
+      self.print_tree(self.right, traversal)
 
-    return self.val
+    return traversal 
 
 class Solution(object):
-  def mergeTrees(self, root1, root2):
+  def mergeTrees(self, root1, root2, traversal=[]):
     """
     :type root1: TreeNode
     :type root2: TreeNode
     :rtype: TreeNode
     """
+    
+    if root1:
+      if root2:
+        traversal.append(root1.val+root2.val)
+      else
+        traversal.append(root1.val)
+    elif root2:
+      traversal.append(root2.val)
 
     if root1.left:
       if root2.left:
-        root1.left = mergeTrees(root1.left, root2.left)
+        root1.left = self.mergeTrees(root1.left, root2.left)
       else:
-        root1.left = mergeTrees(root1.left, TreeNode())
+        root1.left = self.mergeTrees(root1.left, TreeNode())
     elif root1.right:
       if root2.right:
-        root1.right = mergeTrees(root1.right, root2.right)
+        root1.right = self.mergeTrees(root1.right, root2.right)
       else:
-        root1.right = mergeTrees(root1.right, TreeNode())
-      
-    return TreeNode(root1.val+root2.val)
+        root1.right = self.mergeTrees(root1.right, TreeNode())
+
+    return TreeNode(root1.val+root2.val) 
+
+obj = Solution()
+
+root1 = TreeNode(1)
+root1.left = TreeNode(3)
+root1.left.left = TreeNode(5)
+root1.right = TreeNode(2)
+
+root2 = TreeNode(2)
+root2.left = TreeNode(1)
+root2.left.right = TreeNode(4)
+root2.right = TreeNode(3)
+root2.right.right = TreeNode(7)
+
+merged = obj.mergeTrees(root1, root2)
+
+traversal = [3]
+print(root2.left.right.val)
+print(merged.print_tree(traversal))
